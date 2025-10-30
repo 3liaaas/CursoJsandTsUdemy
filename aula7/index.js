0d9351be792754dcc2a1daf511470c052cd61a7b
3liax <contato.elias021021@gmail.com>
2025-10-30 10:10:44 -0300

    Create index.js

---
+++ aula7/index.js
@@ -0,0 +1,17 @@
+//não podemos criar constantes com palavras reservadas
+//constantes precisam ter nomes significativos
+// não pode começar o nome de uma constante com um número
+//não pode conter espaçoes ou traços
+// utilizamos camelCase
+// Case-sensitive
+//não podemos redeclarar variáveis com let
+//NÃO PODE MODIFICAR O VALOR DE UM CONSTANTE
+//não utilize var, use *const*
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const soma = primeiroNumero + segundoNumero;
+const expo = soma ** 2;
+let resultadoTriplicado = expo * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(typeof primeiroNumero)
+
