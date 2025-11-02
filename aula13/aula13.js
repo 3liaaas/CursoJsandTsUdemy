920e9ba2a8e58f23b78d96f1db60e19b88666a19
3liax <contato.elias021021@gmail.com>
2025-11-02 00:27:53 -0300

    manipulando strings

---
+++ aula13/aula13.js
@@ -0,0 +1,5 @@
+// string são indexidas
+// indice começa no 0(zero)
+/**a string ocupa espaço na memória, e acessa os indices pelos [] */
+let umaString = 'Um texto';
+console.log(umaString.charAt(6))
