1c4bcc58841baf55ea8b510603bb5492d6a85c81
3liax <contato.elias021021@gmail.com>
2025-11-01 22:20:54 -0300

    Operadores aritméticos

---
+++ aula10/index.js
@@ -0,0 +1,42 @@
+/*
+-Operadores aritméticos 
++ adição / concatenação 
+(Quando junta STRING + NUMBER, ocorre a concatenação)
+- = subtração
+* = multiplicação
+/ = divisão
+** = potenciação
+% = resto da divisão
+*/ 
+
+/*
+1º ()
+2º **
+3º * / %
+4º + / -
+*/
+
+/**
+ Operador de incremento = let++ / const++
+ Operador de decremento = let-- / const--
+ pré: ++let / ++const
+ pós: let++ / const++
+ */
+
+
+/*Operadores de atribuição */
+// let contador = 2;
+// contador **= 10; // contador = contador + passo
+// console.log(contador)
+
+/*NaN = Not a Number = significa que alguma conta deu errado, cuidado com tipos 
+*parseInt = converte em número inteiro
+*parseFloat = converte em número de ponto flutuante
+*Number = o JS que se vira pra saber o que é
+*/
+
+const n1 = 10;
+const n2 = Number('Elias');
+const s = n1 + n2
+console.log(s)
+console.log(typeof n2)
